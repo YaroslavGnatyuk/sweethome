6967dda36a0653650f95ac215c7c9a5f1055b61e
YaroslavGnatyuk <yaroslavforjob@gmail.com>
2016-08-02 14:47:44 +0300

    1. I added sveral method in service class and have already developed them.

--- src/main/java/ua/gnatyuk/sweethome/services/TemperatureService.java
+++ src/main/java/ua/gnatyuk/sweethome/services/TemperatureService.java
@@ -1,4 +1,21 @@
 package ua.gnatyuk.sweethome.services;
 
+import ua.gnatyuk.sweethome.dto.TemperatureDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TemperatureService {
+    List<TemperatureDTO> getTemperatureDuringOneMonth(LocalDateTime begin, LocalDateTime end);
+    List<TemperatureDTO> getTemperatureDuringOneDay(LocalDateTime begin, LocalDateTime end);
+    List<TemperatureDTO> getTemperatureDuringOneHour(LocalDateTime begin, LocalDateTime end);
+
+    List<TemperatureDTO> getTemperatureDuringNextMonth(LocalDateTime begin, LocalDateTime end);
+    List<TemperatureDTO> getTemperatureDuringPreviousMonth(LocalDateTime begin, LocalDateTime end);
+
+    List<TemperatureDTO> getTemperatureDuringNextDay(LocalDateTime begin, LocalDateTime end);
+    List<TemperatureDTO> getTemperatureDuringPreviousDay(LocalDateTime begin, LocalDateTime end);
+
+    List<TemperatureDTO> getTemperatureDuringNextHour(LocalDateTime begin, LocalDateTime end);
+    List<TemperatureDTO> getTemperatureDuringPreviousHour(LocalDateTime begin, LocalDateTime end);
 }
